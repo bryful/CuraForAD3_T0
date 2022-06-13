3eb659a1db4a30eeaf3e6f20643460710c7dccf1
bryful <bryful@gmail.com>
2022-06-13 18:19:01 +0900

    プロジェクト ファイルを追加します。

---
+++ CuraForAD3_T0/Form1.cs
@@ -0,0 +1,147 @@
+﻿using BRY;
+using Codeplex.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+/// <summary>
+/// 基本となるアプリのスケルトン
+/// </summary>
+namespace CureForAD3_T0
+{
+	public partial class Form1 : Form
+	{
+		//-------------------------------------------------------------
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		/// <summary>
+		/// コントロールの初期化はこっちでやる
+		/// </summary>
+		protected override void InitLayout()
+		{
+			base.InitLayout();
+		}
+		//-------------------------------------------------------------
+		/// <summary>
+		/// フォーム作成時に呼ばれる
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			if (ModifierKeys == 0)
+			{
+				//設定ファイルの読み込み
+				JsonPref pref = new JsonPref();
+				if (pref.Load())
+				{
+					bool ok = false;
+					Size sz = pref.GetSize("Size", out ok);
+					if (ok) this.Size = sz;
+					Point p = pref.GetPoint("Point", out ok);
+					if (ok) this.Location = p;
+				}
+			}
+			this.Text = Path.GetFileNameWithoutExtension(Application.ExecutablePath);
+		}
+		//-------------------------------------------------------------
+		/// <summary>
+		/// フォームが閉じられた時
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			//設定ファイルの保存
+			JsonPref pref = new JsonPref();
+			pref.SetSize("Size", this.Size);
+			pref.SetPoint("Point", this.Location);
+
+			pref.SetIntArray("IntArray", new int[] { 8, 9, 7 });
+			pref.Save();
+
+		}
+		//-------------------------------------------------------------
+		/// <summary>
+		/// ドラッグ＆ドロップの準備
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Form1_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				e.Effect = DragDropEffects.All;
+			}
+			else
+			{
+				e.Effect = DragDropEffects.None;
+			}
+		}
+		/// <summary>
+		/// ドラッグ＆ドロップの本体
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Form1_DragDrop(object sender, DragEventArgs e)
+		{
+			string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+			//ここでは単純にファイルをリストアップするだけ
+			GetCommand(files);
+		}
+		//-------------------------------------------------------------
+		/// <summary>
+		/// ダミー関数
+		/// </summary>
+		/// <param name="cmd"></param>
+		public void GetCommand(string[] cmd)
+		{
+			if (cmd.Length > 0)
+			{
+				foreach (string s in cmd)
+				{
+					textBox1.Text += s;
+				}
+			}
+		}
+		/// <summary>
+		/// メニューの終了
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		//-------------------------------------------------------------
+		private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			AppInfoDialog.ShowAppInfoDialog();
+		}
+		//-------------------------------------------------------------
+		public bool ChnageT0(string p)
+		{
+			bool ret = false;
+			if (File.Exists(p) == false) return ret;
+
+
+
+			return ret;
+		}
+
+
+	}
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# CuraForAD3_T0
+
+3Dプリンタでの必需品であるスライサーのUltimaker Curaを使ってみたら非常にいい感じで、FlashForgeのAdventurer3でも使えないかなと作ったものです。<br>
+[SatoMouse's blogのAdventurer3でCuraを使う](https://satomouse.com/adventurer3-cura/)にあったものをそのままC#で作ったものです。<br>
+<br>
+本当はCuraのプラグインで対処したかったのですが、資料が見つからなかったのでこれを作りました。
+<br>
+Curaの書き出すgcodeファイルはヒートベッドの温度設定のM140/M104行の末尾にT0が付かないので付けるアプリです。
+
+<hr>
+
+## Dependency
+Visual studio 2019 C#
+
+
+
+## License
+This software is released under the MIT License, see LICENSE
+
+## Authors
+
+bry-ful(Hiroshi Furuhashi)
+twitter:[bryful](https://twitter.com/bryful)
+bryful@gmail.com
+
+## References
